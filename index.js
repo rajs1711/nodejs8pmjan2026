d7f625cfd784ca14a7493019cde0b711a7e4392b
rajs1711 <rajsaurav1711@gmail.com>
2026-04-09 20:54:35 +0530

    express setup

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('hello')
-console.log('hello2')
+const express= require('express')// include or require express package in your program
+const app=express()// intiate expres app object
+
+const port=3000
+
+//setup express server.
+app.listen(port,()=>{
+    console.log(`Server started at port number :${port}`)
+})
