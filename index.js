b5396691b2f173bef3ae22e49cf492f6f13ca9ad
rajs1711 <rajsaurav1711@gmail.com>
2026-04-10 21:00:47 +0530

    nodemon and package concept

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app=express()// intiate expres app object
 
 const port=3000
 
+
+
+
 //setup express server.
 app.listen(port,()=>{
-    console.log(`Server started at port number :${port}`)
+    console.log(`Server started at Port number :${port}`);
+    console.log('hello Raj ')
 })
