153ee24c58b14443673f56332a5f4fb7acc336a6
rajs1711 <rajsaurav1711@gmail.com>
2026-04-22 12:51:59 +0530

    api creation

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const app=express()// intiate expres app object
 
 const port=3000
 
+// create one welcome API 
+//https://localhost:3000/api/welcome
+app.get('/api/welcome',(req,res)=>{
+    
+     console.log(req.body)
+    const msg='welcome to our Express Application';
+
+    return res.json({"msg":msg,"status":200}); // here we return data in json format
+});
+// JSON => java script object notation 
+/**
+ * 
+ * {
+ *  "name":"saurav"
+ * 
+ * }
+ */
+
+
 
 
 
 //setup express server.
 app.listen(port,()=>{
     console.log(`Server started at Port number :${port}`);
-    console.log('hello Raj ')
 })
